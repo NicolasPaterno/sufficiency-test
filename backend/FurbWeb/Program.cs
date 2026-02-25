162b2840e1635d899d87513b53a38238bd7926f4
Nícolas Santos <nicolasps.contato@gmail.com>
2026-02-24 23:32:29 -0300

    feat: migration on start

--- backend/FurbWeb/Program.cs
+++ backend/FurbWeb/Program.cs
@@ -94,6 +94,12 @@ builder.Services.AddScoped<ComandaService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
